e668defeabedba37777e632cc9c3e522c66476a3
Magda <supercoder@192.168.100.5>
2024-12-23 14:44:43 +0100

    trinkgeldrechner

---
+++ README.md
@@ -0,0 +1,34 @@
+# Trinkgeldrechner
+
+## Beschreibung
+
+Die Aufgabe bestand darin, einen **Trinkgeldrechner** zu erstellen, der dabei hilft, die Gesamtkosten einer Rechnung inklusive Trinkgeld zu berechnen. 
+Du kannst: 
+
+- Den Betrag auf mehrere Personen aufteilen.
+- Je nach Servicequalität das Trinkgeld anpassen.
+
+Das Projekt wurde mit **TypeScript** entwickelt und mit **HTML** und **CSS** gestaltet.
+
+## Projekt
+
+![screenshot](./public/rechner.png)
+
+## Funktionen
+
+- **Rechnungsbetrag eingeben:** Ermöglicht die Eingabe des Gesamtbetrags der Rechnung.
+- **Anzahl der Personen festlegen:** Verteilt den Betrag auf die angegebene Anzahl von Personen.
+- **Servicequalität auswählen:** Berechnung des Trinkgelds basierend auf:
+  - **Schlechter Service:** 2% Trinkgeld
+  - **Guter Service:** 10% Trinkgeld
+  - **Hervorragender Service:** 20% Trinkgeld
+- **Ergebnisse anzeigen:**
+  - Trinkgeldbetrag
+  - Gesamtkosten inklusive Trinkgeld
+  - Kosten pro Person
+- **Löschen-Funktion:** Setzt alle Eingaben und Ergebnisse zurück.
+
+## Flowchart
+
+![screenshot](./public/flowchart.png)
+
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,51 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const numberInputValue = document.querySelector('#betrag') as HTMLInputElement;
+const personenInputValue = document.querySelector('#personen') as HTMLInputElement;
+const serviceInputValue = document.querySelector('#service') as HTMLInputElement;
+const button = document.querySelector('#btn') as HTMLButtonElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const deletButton = document.querySelector('#delet') as HTMLButtonElement;
+
+// - querySelector nutzt die gleichen kombinierten Selektoren wie CSS
+// - Sucht ein <span>, das sich innerhalb des Elements mit der ID "trinkgeld" befindet -> kann sogar verschachtelt sein
+// -
+const trinkgeldSpan = document.querySelector('#trinkgeld span') as HTMLSpanElement;
+const gesamtBetragSpan = document.querySelector('#gesamtBetrag span') as HTMLSpanElement;
+const proPersonSpan = document.querySelector('#proPeron span') as HTMLSpanElement;
+
+
+function calculate() {
+
+  const betrag = Number(numberInputValue.value)
+  const personen = Number(personenInputValue.value);
+  const service = Number(serviceInputValue.value);
+  
+//- Berechnungen
+  const trinkgeld = betrag * (service / 100); // Trinkgeldbetrag
+  const gesamtBetrag = betrag + trinkgeld; // Gesamtbetrag inkl. Trinkgeld
+  const betragProPerson = gesamtBetrag / personen; // Betrag pro Person
+
+
+  // - fixt das "." und wandelt es in eine "," um
+  const trinkgeldFixed = trinkgeld.toFixed(2).replace('.', ',') + ' €';
+  const gesamtBetragFixed = gesamtBetrag.toFixed(2).replace('.', ',') + ' €';
+  const betragProPersonFixed = betragProPerson.toFixed(2).replace('.', ',') + ' €';
+
+  // - sezt den Text (Betrag) in den angegebenen <span>
+  trinkgeldSpan.innerText = trinkgeldFixed;
+  gesamtBetragSpan.innerText = gesamtBetragFixed;
+  proPersonSpan.innerText = betragProPersonFixed;
+ 
+}
+
+function delet() {
+  numberInputValue.value = '';
+  personenInputValue.value = '';
+  serviceInputValue.value = '';
+  trinkgeldSpan.innerText = '';
+  gesamtBetragSpan.innerText = '';
+  proPersonSpan.innerText = '';
+}
+
+button.onclick = calculate;
+deletButton.onclick = delet
